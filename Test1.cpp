30abdd47041c3146f9c7cd863f32320be20e6060
ardgrauw <arn.degrauwe@gmail.com>
2015-12-03 11:41:31 +0100

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -1,21 +1,28 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
 
+//Change directory naar cplex/include (en niet verder!)
 #include "stdafx.h"
-#include <C:\Ilog\include\ilcplex\ilocplex.h> 
+#include <ilcplex/ilocplex.h> 
 
 //idea for multiple dimensions
 //typedef IloArray<IloNumArray> Xjk;
 //typedef IloArray<Xjk> Xijk;
 //typedef IloArray<Xijk> Xijkl; //xijkl[l][i][j][k]
-
+//from IBM
+typedef IloArray<IloNumVarArray> NumVarMatrix;
+typedef IloArray<NumVarMatrix>   NumVar3Matrix;
+typedef IloArray<IloRangeArray> RangeMatrix;
+typedef IloArray<RangeMatrix> Range3Matrix;
+typedef IloArray<IloConstraintArray> ConstraintMatrix;
+typedef IloArray<ConstraintMatrix> Constraint3Matrix;
 
 
 int _tmain(int argc, _TCHAR* argv[]){
 	//Variables
 	const int J = 10; //number of convertor loads to do
 	const int I = 10; // number of available types
-	// constants also possible using #define 'var name' 'value'
+	// constants also possible using #define const value
 	double c[I]; // cost of scrap type i
 	double V[I][2]; //loading speed
 	double t[J][2]; // time to fill spoon for load j at place k
@@ -28,7 +35,7 @@ int _tmain(int argc, _TCHAR* argv[]){
 	double AStart[I][2]; // initial stock levels
 	double totalWeight[J]; // total weight for load j
 	double W; // weight capacity for a spoon
-	double V; // volume capacity for a spoon
+	double Vol; // volume capacity for a spoon
 	double Y; // number of types that the system can handle
 
 	//test variables
@@ -57,11 +64,17 @@ int _tmain(int argc, _TCHAR* argv[]){
 	IloModel model(env);
 	IloExpr expr(env);
 	//gebruik IloInt in de for-loops?!?
+	NumVar3Matrix w(env, I);
+	NumVar3Matrix y(env, I);
 	for (IloInt i = 0; i < I; ++i){
+		w[i] = NumVarMatrix(env, J);
+		y[i] = NumVarMatrix(env, J);
 		for (IloInt j = 0; j < J; ++j){
+			w[i][j] = IloNumVarArray(env, 2);
+			y[i][j] = IloNumVarArray(env, 2);
 			for (IloInt k = 0; k < 2; ++k){
-				IloNumVar w[i][j][k](env, 0, 50, ILOFLOAT);
-				IloNumVar y[i][j][k](env, 0, 1, ILOBOOL);
+				w[i][j][k] = IloNumVar (env, 0, 50, ILOFLOAT);
+				y[i][j][k] = IloNumVar (env, 0, 1, ILOBOOL);
 				expr += w[i][j][k] * y[i][j][k] * c[i];
 			}
 		}
@@ -72,18 +85,27 @@ int _tmain(int argc, _TCHAR* argv[]){
 	expr.end();
 
 	//Add constraints
+	Constraint3Matrix bigM(env, I);
+	Range3Matrix stock(env, I);
 	for (IloInt i = 0; i < I; ++i){
+		bigM[i] = ConstraintMatrix(env, J);
+		stock[i] = RangeMatrix(env, J);
 		for (IloInt j = 0; j < J; ++j){
+			bigM[i][j] = IloConstraintArray(env, 2);
+			stock[i][j] = IloRangeArray(env, 2);
 			for (IloInt k = 0; k < 2; ++k){
-				IloRange bigM[i][j][k](env, 0.3 * y[i][j][k], w[i][j][k], 50 * y[i][j][k]);
-				IloRange stock[i][j][k](env, 0.0, w[i][j][k], A[i][j][k]);
+				bigM[i][j][k] = IloConstraint( w[i][j][k] <= 50 * y[i][j][k]);
+				stock[i][j][k] = IloRange(env, 0.0, w[i][j][k], A[i][j][k]);
 				model.add(bigM[i][j][k]); model.add(stock[i][j][k]);
 			}
 		}
 	}
 	
-	for (IloInt k = 0; k < 2; ++k){
-		for (IloInt j = 0; j < J; ++j){
+	RangeMatrix Tijd(env, J), Gewicht(env, J), Volume(env, J), Types(env, J);
+	for (IloInt j = 0; j < J; ++j){
+		Tijd[j] = IloRangeArray(env, 2); Gewicht[j] = IloRangeArray(env, 2);
+		Volume[j] = IloRangeArray(env, 2); Types[j] = IloRangeArray(env, 2);
+		for (IloInt k = 0; k < 2 ; ++k){
 			IloExpr tijd(env);
 			IloExpr gewicht(env);
 			IloExpr volume(env);
@@ -94,15 +116,18 @@ int _tmain(int argc, _TCHAR* argv[]){
 				volume += w[i][j][k] * rho[i];
 				types += y[i][j][k];
 			}
-			IloRange Tijd[j][k](env, 0.0, tijd, t[j][k]);
-			IloRange Gewicht[j][k](env, 0.0, gewicht, W);
-			IloRange Volume[j][k](env, 0.0, volume, V);
-			IloRange Types[j][k](env, 0.0, types, Y);
+			Tijd[j][k] = IloRange(env, 0.0, tijd, t[j][k]);
+			Gewicht[j][k] = IloRange(env, 0.0, gewicht, W);
+			Volume[j][k] = IloRange(env, 0.0, volume, Vol);
+			Types[j][k] = IloRange(env, 0.0, types, Y);
 			model.add(Tijd[j][k]); model.add(Gewicht[j][k]); model.add(Volume[j][k]); model.add(Types[j][k]);
 			tijd.end(); gewicht.end(); volume.end(); types.end();
 		}
 	}
 
+	IloRangeArray Residuelen(env, J);
+	IloRangeArray Zwavel(env, J);
+	IloConstraintArray Gewicht2(env, J);
 	for (IloInt j = 0; j < J; ++j){
 		IloExpr gewicht(env);
 		IloExpr residuelen(env);
@@ -114,20 +139,27 @@ int _tmain(int argc, _TCHAR* argv[]){
 				zwavel += w[i][j][k] * S[i];
 			}
 		}
-		IloRange Gewicht[j](env, W[j], gewicht, totalWeight[j]);
-		IloRange Residuelen[j](env, 0.0, residuelen, Re[j]);
-		IloRange Zwavel[j](env, 0.0, zwavel, Su[j]);
+		Gewicht2[j] = IloConstraint(gewicht == totalWeight[j]);
+		Residuelen[j] = IloRange(env, 0.0, residuelen, Re[j]);
+		Zwavel[j] = IloRange(env, 0.0, zwavel, Su[j]);
 		model.add(Gewicht[j]); model.add(Residuelen[j]); model.add(Zwavel[j]);
 		gewicht.end(); residuelen.end(); zwavel.end();
 	}
 
 	//A initialiseren (nog steeds constraint)
+	//Stock heeft als indeces i,k,j ipv i,j,k!
+	Constraint3Matrix Stock(env, I);
+	Constraint3Matrix Av(env, I);
 	for (IloInt i = 0; i < I; ++i){
+		Stock[i] = ConstraintMatrix(env, 2);
+		Av[i] = ConstraintMatrix(env, 2);
 		for (IloInt k = 0; k < 2; ++k){
-			A[i][0][k] = AStart[i][k];
-			for (IloInt j = 0; j < J; ++j){
-				A[i][j][k] = A[i][j - 1][k] - w[i][j - 1][k];
-				IloRange Stock[i][j][k](env, 0.0, w[i][j][k], A[i][j][k]);
+			Stock[i][k] = IloConstraintArray(env, J);
+			Av[i][k] = IloConstraintArray(env, J+1);
+			Av[i][k][0] = IloConstraint(Av[i][k][0] == AStart[i][k]);
+			for (IloInt j = 1; j < J+1; ++j){
+				Av[i][j][k] = IloConstraint(Av[i][j][k] == Av[i][j - 1][k] - w[i][j - 1][k]);
+				Stock[i][k][j] = IloConstraint(w[i][j][k] <= A[i][j][k]);
 				model.add(Stock[i][j][k]);
 			}
 		}
@@ -136,7 +168,7 @@ int _tmain(int argc, _TCHAR* argv[]){
 
 	//solve the model
 	IloCplex(model);
-	solve();
+	IloSolve;
 
 	env.end();
 	
